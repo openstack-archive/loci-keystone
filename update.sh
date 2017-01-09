2dde5fd96f357fe4a5f3d2a0195be2de37ad77ef
Sam Yaple <sam@yaple.net>
2017-01-09 21:15:06 +0000

    Abstract install

--- update.sh
+++ update.sh
@@ -6,4 +6,6 @@ set -u
 for repo in debian ubuntu; do
 	sed -e "s/%%DISTRO%%/${repo}/" downloader.sh > ${repo}/downloader.sh
     chmod +x ${repo}/downloader.sh
+    cp install.sh ${repo}/install.sh
+    cp cleanup.sh ${repo}/cleanup.sh
 done
